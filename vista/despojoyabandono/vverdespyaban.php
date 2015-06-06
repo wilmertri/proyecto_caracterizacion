f9c0cf0fcee47d5d0ae151cba48e002e6d34d184
DiegoDiazz <diegoarmdiaz@gmail.com>
2015-06-06 18:22:37 -0500

    Vista vverdespyaban.php
    Se creo un archivo para la vista de detalle de despojo y abandono de
    tierras

---
+++ vista/despojoyabandono/vverdespyaban.php
@@ -0,0 +1,11 @@
+<?php
+	include ("")
+?>
+<div class="forms1">
+	<h1>Datos de despojo y abandono de tierras</h1>
+	<table class ="tabke">
+		<tr>
+			<th>Lugar de expulsion</tr>
+			<td><?= ?></td>
+		</tr>
+		
