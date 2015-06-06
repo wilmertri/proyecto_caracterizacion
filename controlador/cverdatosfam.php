2ce607149470fd2e679c75d569f1a4296de0348f
Wilmer Triana <fabiantriana1072@gmail.com>
2015-06-06 13:04:03 -0500

    Actualización MVC Datos Composición familiar
    Se crea el controlador para validar los datos del MVC y mostrarlos

---
+++ controlador/cverdatosfam.php
@@ -0,0 +1,127 @@
+<?php
+
+	include ("modelo/mdatosfam.php");
+
+	$datosfamper = new mdatosfam();
+
+	$id 	= isset($_GET["id"]) ? $_GET["id"]:NULL;
+
+	$dato1 	= $datosfamper->selper1($id);
+
+	if ($dato1[0]['numdocfam']) 
+	{
+		//$verTipoVivienda    = $ins->get_valor($datosvivienda[0]['tipvivper']); --> Tipo de uso valor parametro
+		$numdocfam	= $dato1[0]['numdocfam'];
+	}
+	else
+	{
+		$numdocfam	= "Sin registro";
+	}
+
+	if ($dato1[0]['pnomfam']) 
+	{
+		$pnomfam	= $dato1[0]['pnomfam'];
+	}
+	else
+	{
+		$pnomfam	= "Sin registro";
+	}
+
+	if ($dato1[0]['snomfam']) 
+	{
+		$snomfam	= $dato1[0]['snomfam'];
+	}
+	else
+	{
+		$snomfam	= "Sin registro";
+	}
+
+	if ($dato1[0]['papefam']) 
+	{
+		$papefam	= $dato1[0]['papefam'];
+	}
+	else
+	{
+		$papefam	= "Sin registro";
+	}
+
+	if ($dato1[0]['sapefam']) 
+	{
+		$sapefam	= $dato1[0]['sapefam'];
+	}
+	else
+	{
+		$sapefam	= "Sin registro";
+	}
+
+	if ($dato1[0]['fecnamfam']) 
+	{
+		$fechanac	= $dato1[0]['fecnamfam'] . " Edad: " . $datosfamper->calcular_edad($dato1[0]['fecnamfam']) ;
+	}
+	else
+	{
+		$fechanac	= "Sin registro";
+	}
+
+	if ($dato1[0]['genfam']) 
+	{
+		$verGenFam   = $ins->get_valor($dato1[0]['genfam']);
+		$genfam	= $verGenFam[0]['nomval'];
+	}
+	else
+	{
+		$genfam	= "Sin registro";
+	}
+
+	if ($dato1[0]['geslanfam']) 
+	{
+		$verGesLanFam   = $ins->get_valor($dato1[0]['geslanfam']);
+		$geslanfam	= $verGesLanFam[0]['nomval'];
+	}
+	else
+	{
+		$geslanfam	= "Sin registro";
+	}
+
+	if ($dato1[0]['nivescfam']) 
+	{
+		$verNivEsc   = $ins->get_valor($dato1[0]['nivescfam']);
+		$nivescfam	= $verNivEsc[0]['nomval'];
+	}
+	else
+	{
+		$nivescfam	= "Sin registro";
+	}
+
+	if ($dato1[0]['ultgracurfam']) 
+	{
+		$ultgracurfam 	= $$dato1[0]['ultgracurfam'];
+	}
+	else
+	{
+		$ultgracurfam 	= "Sin registro";
+	}
+
+	if ($dato1[0]['ocuactfam']) 
+	{
+		$verOcupacionActual   = $ins->get_valor($dato1[0]['ocuactfam']);
+		$ocupacionActual	= $verOcupacionActual[0]['nomval'];
+	}
+	else
+	{
+		$ocupacionActual	= "Sin registro";
+	}
+
+	if ($dato1[0]['etniaper']) 
+	{
+		$verEtnia   = $ins->get_valor($dato1[0]['etniaper']);
+		$etnia		= $verEtnia[0]['nomval'];
+	}
+	else
+	{
+		$etnia	= "Sin registro";
+	}
+
+
+
+
--- vista/composicionfamiliar/vdetallefamper.php
+++ vista/composicionfamiliar/vdetallefamper.php
@@ -1,79 +1,71 @@
 <?php
-	include ("controlador/cdatosfam.php");
+	include ("controlador/cverdatosfam.php");
 ?>
 <div class="forms1">	
 	<h1>Detalle Datos de los Familires </h1>
 	<table class="table" cellspacing="0" width="100%">
 		<tr>
 			<td>Documento:</td>
-			<td><?php echo $dato1[0]['numdocfam']; ?></td>
+			<td><?= $numdocfam; ?></td>
 		</tr>
 		<tr>
 			<td> Primer Nombre: </td>
-			<td><?php echo $dato1[0]['pnomfam']; ?></td>
+			<td><?= $pnomfam; ?></td>
 		</tr>
 		<tr>
 			<td> Segundo Nombre: </label> </td>
-			<td> <?php echo $dato1[0]['snomfam'] ?></td>
+			<td> <?= $snomfam; ?></td>
 		</tr>
 		<tr>
 			<td> Primer Apellido: </td>
-			<td><?php echo $dato1[0]['papefam'] ?></td>
+			<td><?= $papefam; ?></td>
 		</tr>
 		<tr>
 			<td> Segundo Apellido:</td>
-			<td><?php echo $dato1[0]['sapefam'] ?></td>
+			<td><?= $sapefam; ?></td>
 		</tr>
 		<tr>
 			<td> Fecha de Nacimiento:</td>
-			<td><?php echo $dato1[0]['fecnacfam'] ?></td>
+			<td><?= $fechanac; ?></td>
 		</tr>
 		<tr>
 			<td> Genero: </td>
-				<?php
-					$gen = $ins->valor2($dato1[0]["genfam"]);
-				?>
-			<td> <?php echo $gen[0]['nomval'] ?></td>	
+			<td> <?= $genfam; ?></td>	
 		</tr>
 		<tr>
 			<td> Gestante o Lactante: </td>
-				<?php
-					$gest = $ins->valor2($dato1[0]["geslanfam"]);
-				?>
-			<td> <?php echo $gest[0]['nomval'] ?></td>	
+			<td> <?= $geslanfam; ?></td>	
 		</tr>
 		<tr>
 			<td> Nivel de Escolaridad: </td>
-				<?php
-					$nesc = $ins->valor2($dato1[0]["nivescfam"]);
-				?>
-			<td> <?php echo $nesc[0]['nomval'] ?></td>	
+			<td> <?= $nivescfam; ?></td>	
 		</tr>
 		<tr>
 			<td> Ultimo Grado Cursado: </td>
-			<td><?php echo $dato1[0]['ultgracurfam'] ?></td>
+			<td><?= $ultgracurfam; ?></td>
 		</tr>
 		<tr>
 			<td> Ocupacion: </td>
-			<td><?php echo $dato1[0]['ocuactfam'] ?></td>
+			<td><?= $ocupacionActual; ?></td>
 		</tr>
 		<tr>
 			<td> Etnia: </td>
-				<?php
-					$etn2 = $ins->valor2($dato1[0]["etniaper"]);
-				?>
-			<td> <?php echo $etn2[0]['nomval'] ?></td>	
+			<td> <?= $etnia; ?></td>	
 		</tr>
 		<tr>
 			<td> Parentesco:</td>
 			<?php
-					$parent = $ins->valor2($dato1[0]["parperfam"]);
+					$parent = $datosfamper->valor2($dato1[0]["parperfam"]);
 				?>
-			<td> <?php echo $parent[0]['nomval'] ?></td>	
+			<td> <?= $parent[0]['nomval'] ?></td>	
 		</tr>
 		<tr>
-			<td align = "center"><a href ="home.php?var=7&id=<?php echo $dato1[0]["numdocfam"]; ?>" class="btn btn-primary">Editar</a></td>
-			<td align = "center"><a href ="home.php?var=36&famper=1&id=<?php echo $dato1[0]["numdocfam"]; ?>" class="btn btn-primary">Discapacidad</a></td>
+			<td align = "center">
+				<a href ="home.php?var=7&id=<?= $dato1[0]["numdocfam"]; ?>" class="btn btn-success">Editar</a>
+				<a href="home.php?id=<?= $id ?>&var=70" class="btn btn-success">Volver</a>
+			</td>
+
+			<td align = "center"><a href ="home.php?var=36&famper=1&id=<?= $dato1[0]["numdocfam"]; ?>" class="btn btn-success">Discapacidad</a></td>
 		</tr>
 	</table>
 </div>	
